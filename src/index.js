d8e0cf6b789d1de0fceb75ac7a9c1a2259d74b77
Robert Murray <rpmurray42@gmail.com>
2018-01-05 11:12:05 -0800

    Gutted the original project and connected it to the backend, then i pulled the data from the postges DB and showed it i some header tags

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloClient } from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import Routes from "./routes";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  link: createHttpLink({ uri: "http://localhost:8081/graphql" }),
+  cache: new InMemoryCache()
+});
+
+const App = (
+  <ApolloProvider client={client}>
+    <Routes />
+  </ApolloProvider>
+);
+
+ReactDOM.render(App, document.getElementById("root"));
 registerServiceWorker();
